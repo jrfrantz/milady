b426fac64f7ecf1cb1c70bfe1049157689b00843
Jacob Frantz <jacobrossfrantz@gmail.com>
2022-03-27 22:12:58 -0400

    background gradient + font to helvetica

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        sans: ['Helvetica', ...defaultTheme.fontFamily.sans],
       },
       keyframes: {
         'slide-down': {
