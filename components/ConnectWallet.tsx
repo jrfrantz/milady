e938ee280ad40398d5eede47c975029a1f5664bb
Jacob Frantz <jacobrossfrantz@gmail.com>
2022-03-28 17:30:31 -0400

    support more wallets

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react'
 import { useAccount, useBalance, useConnect } from 'wagmi'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { WalletLinkConnector } from 'wagmi/connectors/walletLink'
 import EthAccount from './EthAccount'
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 import Link from 'next/link'
@@ -12,6 +14,17 @@ const ConnectWallet: FC = () => {
     fetchEns: true,
   })
   const wallet = connectData.connectors[0]
+  const walletConnect = new WalletConnectConnector({
+    options: {
+      qrcode: true
+    }
+  })
+  const walletLink = new WalletLinkConnector({
+    options: {
+      appName: 'Please Be Patient With.Me | Milady Marketplace',
+      jsonRpcUrl: 'https://mainnet.infura.io/v3/4f19d5e3a88d4f2497e7fc227f18a85f'
+    }
+  })
 
   if (accountData) {
     return (
@@ -64,13 +77,66 @@ const ConnectWallet: FC = () => {
   }
 
   return (
-    <button
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger className="btn-primary-fill col-span-2 col-start-3 ml-auto md:col-span-4 md:col-start-5 lg:col-span-4 lg:col-start-9">
+        Connect Wallet
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content
+          align="end"
+          sideOffset={6}
+          className="w-48 space-y-1 rounded bg-white px-1.5 py-2 shadow-md  radix-side-bottom:animate-slide-down md:w-56"
+        >
+          <DropdownMenu.Item asChild>
+            <button
+              key={wallet.id}
+              onClick={() => connect(wallet)}
+              className="group flex w-full cursor-pointer items-center justify-between gap-3 rounded px-4 py-3 outline-none transition hover:bg-neutral-100 focus:bg-neutral-100"
+            >
+            Metamask
+            </button>
+          </DropdownMenu.Item>
+          <DropdownMenu.Item asChild>
+          <button
+            key={wallet.id + '2'}
+            onClick={() => connect(walletConnect)}
+            className="group flex w-full cursor-pointer items-center justify-between gap-3 rounded px-4 py-3 outline-none transition hover:bg-neutral-100 focus:bg-neutral-100"
+          >
+            WalletConnect
+          </button>
+          </DropdownMenu.Item>
+          <DropdownMenu.Item asChild>
+          <button
+            key={wallet.id + '22'}
+            onClick={() => connect(walletLink)}
+            className="group flex w-full cursor-pointer items-center justify-between gap-3 rounded px-4 py-3 outline-none transition hover:bg-neutral-100 focus:bg-neutral-100"
+          >
+            WalletLink
+          </button>
+          </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+    /*<button
       key={wallet.id}
       onClick={() => connect(wallet)}
       className="btn-primary-fill col-span-2 col-start-3 ml-auto md:col-span-4 md:col-start-5 lg:col-span-4 lg:col-start-9"
     >
       Connect Wallet
     </button>
+    <button
+      key={wallet.id + '2'}
+      onClick={() => connect(walletConnect)}
+      className="btn-primary-fill col-span-2 col-start-3 ml-auto md:col-span-4 md:col-start-5 lg:col-span-4 lg:col-start-9"
+    >
+      Connect WalletConnect
+    </button>
+    <button
+      key={wallet.id + '22'}
+      onClick={() => connect(walletLink)}
+      className="btn-primary-fill col-span-2 col-start-3 ml-auto md:col-span-4 md:col-start-5 lg:col-span-4 lg:col-start-9"
+    >
+      Connect WalletLink
+    </button>
+    </>*/
   )
 }
 
---
+++ lib/connectors.ts
@@ -0,0 +1,8 @@
+
+export default function getConnectors() {
+    // TODO: move connectors logic from
+    // ConnectWallet.tsx to here.
+    // just return an array of three items
+    // [wallet, walletConnect, walletLink]
+    // so that the frontend can just map over it.
+}
